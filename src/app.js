b87368183073eef4041aa088bea1847d27648777
heawer <accounts+ghtest@sapphic.moe>
2025-04-26 01:35:56 +0500

    level 2

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 import express from 'express'
 import { v4 as uuid } from 'uuid'
-import { port } from './config'
+import { port } from './config.js'
 
 const app = express()
+app.use(express.json())
 
 const tasks = []
 
-app.use(express.json())
-
-app.get('/', (req, res) => res.send('Hello, world!'))
-
 app.get('/tasks', (req, res) => {
   return res.json({ tasks })
+})
+
+app.get('/tasks/:id', (req, res) => {
+  const { id } = req.params
+  const task = tasks.find(t => t.id === id)
+
+  if (!task) {
+    return res.status(404).json({
+      message: "Task not found"
+    })
+  }
+
+  return res.json({ task })
 })
 
 app.post('/tasks', (req, res) => {
@@ -33,12 +43,53 @@ app.post('/tasks', (req, res) => {
 
   tasks.push(newTask)
 
-  console.log('Task created', newTask)
-
   return res.json({
     message: 'Task created successfully',
     tasks
   })
+})
+
+app.put('/tasks/:id', (req, res) => {
+  const { id } = req.params
+  const taskIndex = tasks.findIndex(t => t.id === id)
+
+  if (taskIndex === -1) {
+    return res.status(404).json({
+      message: "Task not found"
+    })
+  }
+
+  if (!req.body) {
+    return res.status(200).json({
+      message: "You must include a body"
+    })
+  }
+
+  const { description, completed } = req.body
+
+  if (description !== undefined) tasks[taskIndex].description = description
+  if (completed !== undefined) tasks[taskIndex].completed = completed
+  tasks[taskIndex].updatedAt = new Date()
+
+  return res.json({
+    message: "Task updated successfully",
+    task: tasks[taskIndex]
+  })
+})
+
+app.delete('/tasks/:id', (req, res) => {
+  const { id } = req.params
+  const taskIndex = tasks.findIndex(t => t.id === id)
+
+  if (taskIndex === -1) {
+    return res.status(404).json({
+      message: "Task not found"
+    })
+  }
+
+  tasks.splice(taskIndex, 1)
+
+  return res.status(204).send()
 })
 
 app.listen(port, () => console.log(`Server listening on port ${port}`))
